dba6db0a79439c022940138e22b32c7be89ce803
anash5119 <anash5119@gmail.com>
2023-09-20 17:20:57 -0700

    Complete form functionality, start library display

--- script.js
+++ script.js
@@ -9,20 +9,43 @@ function Book(title, author, pages, didRead){
 }
 
 function addBookToLibrary(){
-    let title = prompt("Book Title?");
-    let author = prompt("Author?");
-    let pages = prompt("Number of Pages?");
-    let didRead = prompt("Did you already read it?");
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let didRead = document.getElementById('didRead').value;
 
     myLibrary.push(new Book(title, author, pages, didRead));
 
     displayMyLibrary();
+
+    document.getElementById("bookForm").reset();
+    closeForm();
+
 }
 
 function displayMyLibrary(){
-    // for(let index = 0; index < myLibrary.length; index++){
-    //     console.log(myLibrary[index].title);
-    // }
+    const divCont = document.getElementById('bookContainer');
 
-    myLibrary.forEach((Book) => console.log(Book.title));
+    for(let index = 0; index < myLibrary.length; index++){
+        //console.log(myLibrary[index].title);
+        let newBook = document.createElement("div");
+        newBook.setAttribute('id', 'books'); 
+        let content1 = document.createElement("p");
+        let node1 = document.createTextNode(myLibrary[index].title);
+        content1.appendChild(node1);
+        newBook.appendChild(content1);
+
+        console.log(myLibrary[index].title);
+        divCont.appendChild(newBook)
+    }
+
+    //myLibrary.forEach((Book) => console.log(Book.title));
 }
+
+function openForm() {
+    document.getElementById("formContainer").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("formContainer").style.display = "none";
+  }
